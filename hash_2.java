59fb3782234e26c76a83d66db7deafdb73aad6db
leejuhyeon <esloven3@gmail.com>
2021-05-14 22:41:17 +0900

    Update

--- hash_2.java
+++ hash_2.java
@@ -2,4 +2,21 @@ package code;
 
 public class hash_2 {
 
+	static String[] phone_book = { "119", "97674223", "1195524421" };
+
+	public static void main(String arg[]) {
+		System.out.println(solution(phone_book));
+	}
+
+	public static boolean solution(String[] phone_book) {
+		boolean answer = true;
+
+		for (int i = 0; i < phone_book.length; i++) {
+			
+			
+
+		}
+
+		return answer;
+	}
 }
