4337c6a4210ef0f8f43a9814aa4eb80aa9165ed0
leejuhyeon <esloven3@gmail.com>
2021-05-20 21:47:31 +0900

    Update

--- sort_1.java
+++ sort_1.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.Arrays;
+
 public class sort_1 {
 	static int[] array = { 1, 5, 2, 6, 3, 7, 4 };
 	static int[][] commands = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
@@ -14,7 +16,25 @@ public class sort_1 {
 	}
 
 	public static int[] solution(int[] array, int[][] commands) {
-		int[] answer = {};
-        return answer;
+		int[] answer = new int[commands.length];
+
+		for (int n = 0; n < commands.length; n++) {
+			int i = commands[n][0];
+			int j = commands[n][1];
+			int k = commands[n][2];
+			int arr_size = j - i + 1;
+
+			//int[] arr = Arrays.copyOfRange(array, i - 1, j);
+			
+			int[] arr = new int[arr_size];
+			System.arraycopy(array, i - 1, arr, 0, arr_size);
+			
+			Arrays.sort(arr);
+
+			answer[n] = arr[k - 1];
+
+		}
+
+		return answer;
 	}
 }
---
+++ stack_que_1.java
@@ -0,0 +1,79 @@
+package code;
+
+import java.util.*;
+
+public class stack_que_1 {
+
+	static int[] progresses = { 93, 30, 55 };
+	static int[] speeds = { 1, 30, 5 };
+
+	public static void main(String arg[]) {
+		int[] temp = solution(progresses, speeds);
+
+		for (int i = 0; i < temp.length; i++) {
+			System.out.print(temp[i]);
+		}
+	}
+
+	static Queue<Process> q = new LinkedList<Process>();
+
+	static class Process {
+		int progresses = 0;
+		int speeds = 0;
+
+		Process(int progresses, int speeds) {
+			this.progresses = progresses;
+			this.speeds = speeds;
+		}
+
+		void work() {
+			progresses += speeds;
+		}
+	}
+
+	public static int[] solution(int[] progresses, int[] speeds) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+
+		for (int i = 0; i < progresses.length; i++) {
+
+			Process pro = new Process(progresses[i], speeds[i]);
+
+			q.add(pro);
+		}
+
+		int cnt = 0;
+		boolean start = false;
+
+		while (!q.isEmpty()) {
+
+			Process pro = q.peek();
+
+			if (pro.progresses >= 100) {
+				q.poll();
+				cnt++;
+				start = true;
+				continue;
+			}
+
+			if (start) {
+				ret.add(cnt);
+				cnt = 0;
+				start = false;
+			}
+
+			for (Process p : q) {
+				p.work();
+			}
+		}
+		
+		ret.add(cnt);
+
+		int[] answer = new int[ret.size()];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = ret.get(i);
+		}
+
+		return answer;
+	}
+}
