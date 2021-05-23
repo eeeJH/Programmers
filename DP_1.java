dc24077e9f9410bdfc69af9a06fc8d214bb0ebba
leejuhyeon <esloven3@gmail.com>
2021-05-23 23:59:52 +0900

    Udpate

---
+++ DP_1.java
@@ -0,0 +1,17 @@
+package code;
+
+public class DP_1 {
+
+	static int N = 5;
+	static int number = 12;
+
+	public static void main(String arg[]) {
+		System.out.println(solution(N, number));
+	}
+
+	public static int solution(int N, int number) {
+		int answer = 0;
+		return answer;
+	}
+
+}
